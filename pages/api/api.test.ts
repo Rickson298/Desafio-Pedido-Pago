0c1b6d335602195fca6aaedd190c8e2a8e58a981
Rickson Oliveira <rickson.patrick298@gmail.com>
2022-02-22 20:47:39 -0300

    Tests API
    Feature: teste apis's endpoints by Jest

---
+++ pages/api/api.test.ts
@@ -0,0 +1,26 @@
+import fetch from "cross-fetch";
+
+let baseUrl = "https://pp-api-desafio.herokuapp.com";
+
+describe("Test Api", () => {
+  it("Agents Endpoint", async () => {
+    const response = await fetch(`${baseUrl}/agents`);
+    expect(response.status).toBe(200);
+  });
+
+  it("Single agent Endpoint", async () => {
+    const response = await fetch(`${baseUrl}/agent/1`);
+    expect(response.status).toBe(200);
+  });
+
+  it("Roles Endpoint", async () => {
+    const response = await fetch(`${baseUrl}/roles`);
+    expect(response.status).toBe(200);
+  });
+  it("Single Role Endpoint", async () => {
+    const response = await fetch(`${baseUrl}/role/1`);
+    expect(response.status).toBe(200);
+  });
+});
+
+export {};
