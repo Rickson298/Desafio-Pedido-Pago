d828b90d258bda30a8b98863038c6d4c5e850a05
Rickson Oliveira <rickson.patrick298@gmail.com>
2022-02-23 21:50:31 -0300

    Fix: remove unecessary component

--- components/sidebar/SideBar.tsx
+++ components/sidebar/SideBar.tsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const SideBar = styled.aside`
   min-height: 100%;
-  min-width: 256px;
+  width: 256px;
+  background: white;
   border: 1px solid #eaefed;
   box-shadow: 0px 4px 8px rgba(165, 171, 179, 0.16);
   @media (max-width: 1000px) {
